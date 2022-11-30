9b27dc2406790877251e4ba17b870cf1e99991c6
wescorner <wescorner@gmail.com>
2022-11-30 13:07:25 -0500

    use environment variable for token

--- bot.py
+++ bot.py
@@ -1,8 +1,14 @@
 import hikari
 import lightbulb
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 
 bot = lightbulb.BotApp(
-    token=,
+    token=os.environ.get("API_TOKEN"),
     default_enabled_guilds=(1047002328684838982)
 )
 
@@ -14,7 +20,6 @@ async def ping(context):
     prev = context.member.nickname
     result = int(prev) + context.options.num
     await context.member.edit(nickname = result)
-    # await context.respond(prev)
     await context.respond(f'changed nickname to  {str(result)}')
 
 @bot.command
